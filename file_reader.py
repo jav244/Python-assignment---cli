77af12488f36530ed28cf8f335e90c9d3b860094
jav244 <jhvm92@gmail.com>
2017-06-07 16:23:57 +1200

    changed Abstract Factory to Strategy

--- file_reader.py
+++ file_reader.py
@@ -3,124 +3,95 @@ import csv
 import pathlib
 from abc import ABCMeta, abstractmethod
 
-
+# client
 class FileReader:
     def __init__(self, file):
-        self.file = file
-        self.full_data_dics = []
+        self.__file = file
+        self.__full_data_dics = []
 
     def read_file(self):
-        if pathlib.Path(self.file).suffix == ".txt":
-            txt_file = TxtFactory()
-            self.reader(txt_file, self.file)
-        elif pathlib.Path(self.file).suffix == ".csv":
-            csv_file = CsvFactory()
-            self.reader(csv_file, self.file)
+        if pathlib.Path(self.__file).suffix == ".txt":
+            txt_file = TxtRead(self.__file)
+            txt_read = Reader(txt_file)
+            self.__full_data_dics = txt_read.return_file_data()
+        elif pathlib.Path(self.__file).suffix == ".csv":
+            csv_file = CsvRead(self.__file)
+            csv_read = Reader(csv_file)
+            self.__full_data_dics = csv_read.return_file_data()
         else:
             raise Exception('Unknown file type')
-        return self.full_data_dics
-
-    def reader(self, factory, file):
-        read = factory.read()
-        data_set = read.read_file(file)
-
-        catalogue = factory.organize()
-        self.full_data_dics = catalogue.organize(data_set)
+        return self.__full_data_dics
 
 
-# abstract factory
-class FileFactory(metaclass=ABCMeta):
+# context
+class Reader:
+    def __init__(self, type):
+        self.file_type = type
+
+    def return_file_data(self):
+        data = self.file_type.read()
+        return data
+
+
+# interface for strategy
+class AbstractFileRead(metaclass=ABCMeta):
     @abstractmethod
     def read(self):
         pass
 
-    @abstractmethod
-    def organize(self):
-        pass
 
+# concrete strategy 1
+class TxtRead(AbstractFileRead):
+    def __init__(self, the_file):
+        self.__full_data_dics = []
+        self.__file = the_file
 
-# concrete factory 1
-class CsvFactory(FileFactory):
     def read(self):
-        return CsvRead()
+        with open(self.__file, 'r') as f:
+            f = f.read()
+            data_sets = f.split("\n")
+            for x in data_sets:
+                self.__data_splitter(x)
+            return self.__full_data_dics
 
-    def organize(self):
-        return CsvCatalogue()
+    def __data_splitter(self, data_set):
+        lines = re.split(",|/|\s", data_set)
+        data_dic = {'Empid': lines[0],
+                    'Gender':lines[1],
+                    'Age': lines[2],
+                    'Sales': lines[3],
+                    'BMI': lines[4],
+                    'Salary': lines[5],
+                    'Birthday': lines[6]}
+
+        self.__full_data_dics.append(data_dic)
 
 
-# concrete factory 2
-class TxtFactory(FileFactory):
+# concrete strategy 2
+class CsvRead(AbstractFileRead):
+    def __init__(self, the_file):
+        self.__full_data_dics = []
+        self.__file = the_file
+
     def read(self):
-        return TxtRead()
+        with open(self.__file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.__data_splitter_csv(row)
+            return self.__full_data_dics
 
-    def organize(self):
-        return TxtCatalogue()
+    def __data_splitter_csv(self, row):
+        data_dic = {'Empid': row['Empid'],
+                    'Gender': row['Gender'],
+                    'Age': row['Age'],
+                    'Sales': row['Sales'],
+                    'BMI': row['BMI'],
+                    'Salary': row['Salary'],
+                    'Birthday': row['Birthday']}
 
-
-# abstract product A
-class Read(metaclass=ABCMeta):
-    @abstractmethod
-    def read_file(self):
-        pass
-
-
-# concrete product A1
-class CsvRead(Read):
-    def read_file(self, the_file):
-        data_set = csv.DictReader(open(the_file))
-        return data_set
+        self.__full_data_dics.append(data_dic)
 
 
-
-# concrete product A2
-class TxtRead(Read):
-    def read_file(self, the_file):
-        with open(the_file, 'r') as f:
-            file = f.read()
-            data_sets = file.split("\n")
-            return data_sets
-
-
-# abstract product B
-class Catalogue(metaclass=ABCMeta):
-    @abstractmethod
-    def organize(self):
-        pass
-
-
-# concrete product B1
-class CsvCatalogue(Catalogue):
-    def organize(self, data_set):
-        full_data_dic = []
-        for row in data_set:
-            data_dic = {'Empid': row['Empid'],
-                        'Gender': row['Gender'],
-                        'Age': row['Age'],
-                        'Sales': row['Sales'],
-                        'BMI': row['BMI'],
-                        'Salary': row['Salary'],
-                        'Birthday': row['Birthday']}
-
-            full_data_dic.append(data_dic)
-        return full_data_dic
-
-
-# concrete product B2
-class TxtCatalogue(Catalogue):
-    def organize(self, data_set):
-        full_data_dic = []
-        for row in data_set:
-            lines = re.split(",|/|\s", row)
-            data_dic = {'Empid': lines[0],
-                        'Gender': lines[1],
-                        'Age': lines[2],
-                        'Sales': lines[3],
-                        'BMI': lines[4],
-                        'Salary': lines[5],
-                        'Birthday': lines[6]}
-
-            full_data_dic.append(data_dic)
-        return full_data_dic
 
 
 
